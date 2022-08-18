cd332ce3a9ff05da3db0a58ea38cc9be999e07e0
PedroL <pedrolucassantosferreira426@gmail.com>
2022-08-18 17:24:11 -0300

    init

---
+++ components/Navbar.tsx
@@ -0,0 +1,40 @@
+import styles from '../styles/Navbar.module.scss';
+import {AiOutlineShop} from "react-icons/ai";
+import {BiSearchAlt} from "react-icons/bi";
+import {BsCart3, BsHeart, BsPerson} from "react-icons/bs";
+
+const Navbar = () => {
+    return (
+        <header className={styles.header}>
+            <div className={`${styles.container}`}>
+                <div className={`${styles.topHeader}`}>
+                    <div className={styles.startSide}>
+                        <div className={styles.logo}>
+                            <AiOutlineShop fontSize={'50px'}/>
+                            <h1>Online Shop</h1>
+                        </div>
+                    </div>
+                    <div className={styles.endSide}>
+                        <BsCart3/>
+                        <BsHeart/>
+                        <BsPerson/>
+                    </div>
+                </div>
+                <nav className={styles.nav}>
+                    <ul>
+                        <li><a>Home</a></li>
+                        <li><a>Produtos</a></li>
+                        <li><a>Contato</a></li>
+                    </ul>
+                    <div className={styles.search}>
+                        <input placeholder={'Pesquisar...'}/>
+                        <button>
+                            <BiSearchAlt className={styles.searchIcon}/>
+                        </button>
+                    </div>
+                </nav>
+            </div>
+        </header>
+    );
+};
+export default Navbar;
